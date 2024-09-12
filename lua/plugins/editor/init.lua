6241e1899559afec0f066ec63216beefcc838c87
Meshwil Anthony Dias <02mesh@duck.com>
2024-09-12 10:44:08 +0100

    LSP Refactors and Snippets

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -1,3 +1,3 @@
 return {
-  { import = { "plugins.editor" } },
+  { import = "plugins.editor" },
 }
--- lua/plugins/editor/oil.lua
+++ lua/plugins/editor/oil.lua
@@ -3,6 +3,10 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
+  config = function()
+    local oil = require("oil")
+    oil.setup({})
+  end,
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
